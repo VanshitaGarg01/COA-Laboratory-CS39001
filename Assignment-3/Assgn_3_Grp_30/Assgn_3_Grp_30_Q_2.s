749c4da66fff55be762f29bb0ebd4a40b80df2aa
ashutoshaks <ashu11.03.2002@gmail.com>
2021-09-14 03:01:19 +0530

    All 3 working, need to add comments

--- Assignment-3/Assgn_3_Grp_30/Assgn_3_Grp_30_Q_2.s
+++ Assignment-3/Assgn_3_Grp_30/Assgn_3_Grp_30_Q_2.s
@@ -4,168 +4,172 @@
 # Group No. - 30
 # Group Members - Vanshita Garg (19CS10064) & Ashutosh Kumar Singh (19CS30008)
 
+# Data Segment
     .data
-array_prompt:
+array_prompt:                                           # Prompt for array input
     .asciiz "Enter 10 integers:\n"
-k_prompt:
+k_prompt:                                               # Prompt for accepting k
     .asciiz "Enter the value of k: "
-sorted_msg:
+sorted_msg:                                             # Display message for printing the sorted array
     .asciiz "Sorted array: "
-error_msg:
+error_msg:                                              # Error message when sanity check fails
     .asciiz "Error, k cannot be greater than n\n"
-output_msg:
+output_msg:                                             # Display message for output
     .asciiz "k-th largest number is: "
-whitespace:
+whitespace:                                             # Whitespace character
     .asciiz " "
-newline:
+newline:                                                # Newline character
     .asciiz "\n"
-array:
+array:                                      # allocate space for 10 integers (10 * 4 = 40 bytes)
     .align 2
     .space 40
+
+# Code Segment
 
     .text
     .globl main
 
 main:
     # Read 10 nos in array
-    li      $v0, 4                          # $v0 = 4, 4 is the system call code for printing a string to the console
-    la      $a0, array_prompt                     # load address of prompt in $a0
-    syscall                                 # print prompt on the console
+    li      $v0, 4
+    la      $a0, array_prompt
+    syscall                                 # print array_prompt on the console
 
-    la      $t0, array
-    li      $t1, 0
-    li      $s0, 10
+    la      $t0, array                      # load address of array in $t0
+    li      $t1, 0                          # i = 0, loop variable for taking array input
+    li      $s0, 10                         # n = 10, number of elements in the array
 
+# Read the numbers of the array as input
 read_loop:
-    bge     $t1, $s0, read_k
+    bge     $t1, $s0, read_k                # if i >= n, exit the loop and jump to read_k
     li      $v0, 5
-    syscall
-    sw      $v0, 0($t0)
-    addi    $t0, 4
-    addi    $t1, 1
-    j       read_loop
+    syscall                                 # read an integer
+    sw      $v0, 0($t0)                     # store the integer in the appropriate location in the array
+    addi    $t0, 4                          # increment $t0 by 4 to point to the next location in the array
+    addi    $t1, 1                          # i = i + 1
+    j       read_loop                       # jump to read_loop (continue the loop)
 
+# Read the value of k as input
 read_k:
     li      $v0, 4
     la      $a0, k_prompt
-    syscall
+    syscall                                 # print k_prompt on the console
     li      $v0, 5
-    syscall
+    syscall                                 # read the value of k, gets stored in $v0
 
 call_function:
-    move    $s1, $v0        # useless
-    la      $a0, array
-    li      $a1, 10
-    move    $a2, $v0
-
-    jal     find_k_largest
-    move    $t1, $v0
+    move    $s1, $v0                        # store k in $s1
+    la      $a0, array                      # load address of array in $a0            
+    li      $a1, 10                         # $a1 = 10 (size of array)
+    move    $a2, $v0                        # $a2 = $v0 (= k)
+    jal     find_k_largest                  # call the function find_k_largest with $a0, $a1 and $a2 as arguments
+    move    $t1, $v0                        # store the return value in $t1 (the k-th largest element)
 
 print_result:
     li      $v0, 4
-    la      $a0, output_msg
-    syscall
+    la      $a0, output_msg                 
+    syscall                                 # print output_msg on the console
 
     li      $v0, 1
     move    $a0, $t1
-    syscall
+    syscall                                 # print the k-th largest element
 
     li      $v0, 4
     la      $a0, newline
-    syscall
+    syscall                                 # print newline
 
-    j       exit
+    j       exit                            # jump to exit
 
-
+# Function to find the k-th largest number in the array
 find_k_largest:
-    bgt     $a2, $a1, sanity_fail
-    addi    $sp, $sp, -16
-    sw      $a0, 12($sp)
-    sw      $a1, 8($sp)
-    sw      $a2, 4($sp)
-    sw      $ra, 0($sp)
+    bgt     $a2, $a1, sanity_fail           # if k > n, sanity check fails
+    addi    $sp, $sp, -16                   # we need to save the arguments and return address on the stack, so allocate memory for them
+    sw      $a0, 12($sp)                    # save $a0 (address of array) on stack
+    sw      $a1, 8($sp)                     # save $a1 (n) on stack
+    sw      $a2, 4($sp)                     # save $a2 (k) on stack
+    sw      $ra, 0($sp)                     # save $ra on stack
 
-    jal     sort_array
+    jal     sort_array                      # call the function sort_array with $a0, $a1 as arguments
 
-    # add logic to find kth element
-    lw      $t0, 12($sp)
-    sub     $t1, $a1, $a2
-    sll     $t2, $t1, 2
-    add     $t2, $t2, $t0
-    lw      $v0, 0($t2)
+    lw      $t0, 12($sp)                    # load address of first element of array in $t0
+    sub     $t1, $a1, $a2                   # $t1 = n - k, we want to find array[n - k]
+    sll     $t2, $t1, 2                     # $t2 = $t1 * 4
+    add     $t2, $t2, $t0                   # $t2 is the address of array[n - k]
+    lw      $v0, 0($t2)                     # $v0 = array[n - k]
 
-    lw      $ra, 0($sp)
-    addi    $sp, $sp, 16
-    jr      $ra
+    lw      $ra, 0($sp)                     # load the return address into $ra
+    addi    $sp, $sp, 16                    # restore the stack pointer
+    jr      $ra                             # jump to return address
 
-
+# Function to sort the array
+# $t0 stores address of first element of array
+# $t1 stores n
+# $t2 stores j (outer loop variable)
+# $t3 stores i (inner loop variable)
 sort_array:
-    move    $t0, $a0    # t0 = array
-    move    $t1, $a1    # t1 = n
-    li      $t2, 1      # t2 = j = 1
+    move    $t0, $a0                        # $t0 = array
+    move    $t1, $a1                        # $t1 = n
+    li      $t2, 1                          # $t2 = 1
 
 outer_j_loop:
-    bge     $t2, $t1, exit_outer    # exit loop when j >= n
-    sll     $t4, $t2, 2
-    add     $t4, $t4, $t0       # t4 = address of a[j]
-    lw      $t5, 0($t4)         # t5 = temp = a[j]
-    addi    $t3, $t2, -1        #t3 = i = j - 1
+    bge     $t2, $t1, exit_outer            # if j >= n, exit the outer loop
+    sll     $t4, $t2, 2                     # $t4 = $t2 * 4
+    add     $t4, $t4, $t0                   # $t4 = address of array[j]
+    lw      $t5, 0($t4)                     # $t5 =  array[j] (temp)
+    addi    $t3, $t2, -1                    # $t3 = $t2 - 1 (equivalent to i = j - 1)
 
 inner_i_loop:
-    bltz    $t3, exit_inner
-    sll     $t4, $t3, 2
-    add     $t4, $t4, $t0       # t4 = address of a[i]
-    lw      $t6, 0($t4)         # t6 = a[i]
-    ble     $t6, $t5, exit_inner
-    addi    $t4, 4
-    sw      $t6, 0($t4)         # a[i + 1] = a[i]
-    addi    $t3, $t3, -1        # i--
-    j       inner_i_loop
+    bltz    $t3, exit_inner                 # if i < 0, exit the inner loop
+    sll     $t4, $t3, 2                     # $t4 = $t3 * 4
+    add     $t4, $t4, $t0                   # $t4 = address of array[i]
+    lw      $t6, 0($t4)                     # $t6 = array[i]
+    ble     $t6, $t5, exit_inner            # if array[i] <= temp, exit the inner loop
+    addi    $t4, 4                          # $t4 = $t4 + 4, now  $t4 = address of array[i + 1]
+    sw      $t6, 0($t4)                     # array[i + 1] = array[i]
+    addi    $t3, $t3, -1                    # $t3 = $t3 - 1 (equivalent to i = i - 1)
+    j       inner_i_loop                    # continue the inner loop
 
 exit_inner:
-    addi    $t3, $t3, 1         
-    sll     $t4, $t3, 2
-    add     $t4, $t4, $t0
-    sw      $t5, 0($t4)         # a[i + 1] = temp
+    addi    $t3, $t3, 1                     # $t3 now stores i + 1    
+    sll     $t4, $t3, 2                     # $t4 = $t3 * 4
+    add     $t4, $t4, $t0                   # $t4 = address of array[i + 1]
+    sw      $t5, 0($t4)                     # array[i + 1] = temp
 
-    addi    $t2, $t2, 1
-    j       outer_j_loop
+    addi    $t2, $t2, 1                     # $t2 = $t2 + 1 (equivalent to j = j + 1)
+    j       outer_j_loop                    # continue the outer loop
 
 exit_outer:
-    li      $t3, 0          # i = 0
+    li      $t3, 0                          # set loop variable i = 0
 
+# Now we print the sorted array
 print_array:
     li      $v0, 4
     la      $a0, sorted_msg
-    syscall
-
-    # remove this
-    # la      $t0, array
+    syscall                                 # print sorted_msg on the console
 
 print_loop:
-    bge     $t3, $t1, return
+    bge     $t3, $t1, return                # if i >= n, jump to return
     li      $v0, 1
-    lw      $a0, 0($t0)
-    syscall
+    lw      $a0, 0($t0)                     # load the element from array
+    syscall                                 # print the element
     li      $v0, 4
-    la      $a0, whitespace
-    syscall
-    addi    $t0, 4
-    addi    $t3, 1
-    j       print_loop
+    la      $a0, whitespace                 
+    syscall                                 # print a space
+    addi    $t0, 4                          # make $t0 point to the next array element
+    addi    $t3, 1                          # i = i + 1
+    j       print_loop                      # continue the loop
 
 return:
     li      $v0, 4
     la      $a0, newline
-    syscall
-
-    jr      $ra
+    syscall                                 # print a newline
+    jr      $ra                             # jump to the return address
 
 sanity_fail:
-    li      $v0, 4                          # $v0 = 4
-    la      $a0, error_msg                  # load address of limit_msg in $a0
-    syscall                                 # print limit_msg on the console
+    li      $v0, 4
+    la      $a0, error_msg
+    syscall                                 # print error_msg on the console
 
 exit:
-    li      $v0, 10                         # syscall for exit
-    syscall
+    li      $v0, 10                         
+    syscall                                 # syscall for exit
--- Assignment-3/Assgn_3_Grp_30/Assgn_3_Grp_30_Q_3.s
+++ Assignment-3/Assgn_3_Grp_30/Assgn_3_Grp_30_Q_3.s
@@ -4,3 +4,195 @@
 # Group No. - 30
 # Group Members - Vanshita Garg (19CS10064) & Ashutosh Kumar Singh (19CS30008)
 
+    .data
+input_prompt:
+    .asciiz "Enter four positive integers m, n, a and r: \n"
+A_msg:
+    .asciiz "\nMatrix A: \n"
+B_msg:
+    .asciiz "\nMatrix B (Transpose of matrix A): \n"
+whitespace:                                             # Whitespace character
+    .asciiz " "
+newline:                                                # Newline character
+    .asciiz "\n"
+
+    .text
+    .globl main
+
+main:
+    jal     initStack
+    move    $s0, $sp
+
+    li      $v0, 4
+    la      $a0, input_prompt
+    syscall                                 # print input_prompt on the console
+
+    li      $v0, 5
+    syscall                                 # read m
+    move    $a0, $v0
+    jal     pushToStack
+
+    li      $v0, 5
+    syscall                                 # read n
+    move    $a0, $v0
+    jal     pushToStack
+
+    li      $v0, 5
+    syscall                                 # read a
+    move    $a0, $v0
+    jal     pushToStack
+
+    li      $v0, 5
+    syscall                                 # read r
+    move    $a0, $v0
+    jal     pushToStack
+
+    # Allocate memory for matrix A
+    lw      $t0, 0($s0)                     # $t0 = m
+    lw      $t1, -4($s0)                    # $t1 = n
+    mul     $a0, $t0, $t1
+    jal     mallocInStack
+    move    $s1, $v0                        # $s1 = address of first element of matrix A
+
+    # Allocate memory for matrix B
+    lw      $t0, 0($s0)                     # $t0 = m
+    lw      $t1, -4($s0)                    # $t1 = n
+    mul     $a0, $t0, $t1
+    jal     mallocInStack
+    move    $s2, $v0                        # $s2 = address of first element of matrix B
+
+populate_A:
+    lw      $t0, 0($s0)                     # $t0 = m
+    lw      $t1, -4($s0)                    # $t1 = n
+    mul     $t2, $t0, $t1                   # $t2 = m * n
+    li      $t3, 0                          # i = 0
+    lw      $t4, -8($s0)                    # $t4 = a
+    lw      $t5, -12($s0)                   # $t5 = r
+    move    $t6, $s1                        # $t6 = address of A
+
+populate_loop:
+    bge     $t3, $t2, display_A
+    sw      $t4, 0($t6)
+    mul     $t4, $t4, $t5
+    addi    $t6, -4
+    addi    $t3, 1
+    j populate_loop
+
+display_A:
+    li      $v0, 4
+    la      $a0, A_msg
+    syscall                                 # print A_msg on the console
+
+    lw      $a0, 0($s0)                     # $a0 = m
+    lw      $a1, -4($s0)                    # $a1 = n
+    move    $a2, $s1                        # $a2 = address of A
+    jal     printMatrix
+
+call_transpose:
+    lw      $a0, 0($s0)                     # $a0 = m
+    lw      $a1, -4($s0)                    # $a1 = n
+    move    $a2, $s1                        # $a2 = address of A
+    move    $a3, $s2                        # $a3 = address of B
+    jal     transposeMatrix
+
+display_B:
+    li      $v0, 4
+    la      $a0, B_msg
+    syscall                                 # print B_msg on the console
+
+    lw      $a0, -4($s0)                    # $a0 = n
+    lw      $a1, 0($s0)                     # $a1 = m
+    move    $a2, $s2                        # $a2 = address of B
+    jal     printMatrix
+
+free_stack:
+    move    $sp, $fp    
+    j       exit
+
+initStack:
+    addi    $sp, $sp, -4
+    sw      $fp, 0($sp)
+    move    $fp, $sp
+    jr      $ra
+
+pushToStack:
+    addi    $sp, $sp, -4
+    sw      $a0, 4($sp)
+    jr      $ra
+
+mallocInStack:
+    sll     $t0, $a0, 2
+    move    $v0, $sp
+    sub     $sp, $sp, $t0
+    jr      $ra
+
+printMatrix:
+    move    $t0, $a2                        # address of A
+    move    $t3, $a0                        # m
+    move    $t4, $a1                        # n
+
+    li      $t1, 0                          # (i = 0) current row number (0 to m - 1)
+    
+print_outer_loop:
+    beq     $t1, $t3, return_from_print
+    li      $t2, 0                          # (j = 0) current column number (0 to n - 1)
+
+print_inner_loop:
+    beq     $t2, $t4, exit_print_inner_loop
+    li      $v0, 1
+    lw      $a0, 0($t0)                     
+    syscall                                 # print the array element
+    li      $v0, 4
+    la      $a0, whitespace
+    syscall                                 # print whitespace
+    addi    $t0, -4;
+    addi    $t2, 1                          # j = j + 1
+    j       print_inner_loop
+
+exit_print_inner_loop:    
+    li      $v0, 4
+    la      $a0, newline
+    syscall                                 # print newline
+    addi    $t1, 1                          # i = i + 1
+    j       print_outer_loop
+
+return_from_print:
+    jr      $ra
+
+
+transposeMatrix:
+    move    $t0, $a2                        # address of A
+    move    $t1, $a3                        # address of B
+    move    $t2, $a0                        # m
+    move    $t3, $a1                        # n
+
+    li      $t4, 0                          # i = 0
+
+transpose_outer_loop:
+    beq     $t4, $t2, return_from_transpose
+    li      $t5, 0                          # (j = 0) current column number (0 to n - 1)
+
+transpose_inner_loop:
+    beq     $t5, $t3, exit_transpose_inner_loop
+    lw      $t6, 0($t0)                     # A[i][j]
+    mul     $t7, $t2, $t5                   # $t7 = m * j
+    add     $t7, $t7, $t4                   # $t7 = m * j + i
+    sll     $t7, $t7, 2
+    sub     $t7, $t1, $t7
+    sw      $t6, 0($t7)
+    addi    $t0, -4;
+    addi    $t5, 1                          # j = j + 1
+    j       transpose_inner_loop
+
+exit_transpose_inner_loop:    
+    addi    $t4, 1                          # i = i + 1
+    j       transpose_outer_loop
+
+return_from_transpose:
+    jr      $ra
+
+
+exit:
+    li      $v0, 10                         
+    syscall                                 # syscall for exit
+    
