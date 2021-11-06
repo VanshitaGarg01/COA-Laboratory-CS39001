e85128c351bba0bd08b697bea49f97de867f4c63
ashutoshaks <ashu11.03.2002@gmail.com>
2021-11-06 22:31:03 +0530

    Add branch unit and tb

---
+++ Assignment-7/branch_unit_tb.v
@@ -0,0 +1,41 @@
+module branch_unit_tb;
+
+    // Inputs
+    reg [31:0] nextPC;
+    reg [25:0] label0;
+    reg [15:0] label1;
+    reg [31:0] rsAddr;
+    reg lblSel;
+    reg jumpAddr;
+    reg branch;
+    reg validJump;
+
+    // Outputs
+    wire [31:0] nextAddr;
+
+    branch_unit BU (
+        .nextPC(nextPC),
+        .label0(label0),
+        .label1(label1),
+        .rsAddr(rsAddr),
+        .lblSel(lblSel),
+        .jumpAddr(jumpAddr),
+        .branch(branch),
+        .validJump(validJump),
+        .nextAddr(nextAddr)
+    );
+
+    initial begin
+        $monitor("nextAddr = %d", nextAddr);
+
+        nextPC = 32'd200; label0 = 26'd100; label1 = 16'd50; rsAddr = 32'd400; 
+        lblSel = 1'b0; jumpAddr = 1'b0; branch = 1'b1; validJump = 1'b1;
+
+        #5 lblSel = 1'b1;
+        #5 jumpAddr = 1'b1;
+        #5 branch = 1'b0;
+
+        #5 $finish;
+    end
+
+endmodule
