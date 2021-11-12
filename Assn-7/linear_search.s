6afe9d5d1e4ee0ad66fe1ab69f85492a159b6fb3
VanshitaGarg01 <vanshitagarg1707@gmail.com>
2021-11-12 17:54:14 +0530

    kuch kuch

--- Assn-7/linear_search.s
+++ Assn-7/linear_search.s
@@ -4,14 +4,14 @@ main:
     xor $21, $21
     addi $21, 10        # $21 = n = 10
     xor $8, $8          # $8 = i = 0
-    lw $9, -4($20)      # $9 = elem
+    lw $9, -4($20)      # $9 = key
 
 fori:
-    xor $10, $10        # label 6*4
+    xor $10, $10        
     add $10, $8
     comp $11, $21
     add $10, $11        # $10 = i - n
-    bz $10, notFound
+    bz $10, notFound    # if i == n, jump to notFound
 
     xor $12, $12
     add $12, $8
@@ -23,19 +23,19 @@ fori:
     comp $15, $13
     add $14, $9
     add $14, $15        # key - arr[i]
-    bz $14, found
+    bz $14, found       # if key == arr[i], jump to found
 
-    addi $8, 1
+    addi $8, 1          # i = i + 1
     b fori
 
 found: 
     xor $19, $19
-    add $19, $8
+    add $19, $8         # store the index where key is found in $19
     b exit
 
 notFound:
     xor $19, $19
-    addi $19, -1
+    addi $19, -1        # if key is not found, store -1 in $19
 
 exit:
     xor $16, $16
